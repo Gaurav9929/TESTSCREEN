f92606fd1912084251d694c641877c70a97f5c7c
Omega Solutions <gauravbangad33@gmail.com>
2016-06-03 11:12:16 +0530

    hello

---
+++ DEMOGIT/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  DEMOGIT
+//
+//  Created by Omega Solutions on 03/06/16.
+//  Copyright © 2016 Omega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *trialObj;
+
+
+@end
+
---
+++ DEMOGIT/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  DEMOGIT
+//
+//  Created by Omega Solutions on 03/06/16.
+//  Copyright © 2016 Omega. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "TrialScreen.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)trialMethod:(id)sender
+{
+    TrialScreen * next=[self.storyboard instantiateViewControllerWithIdentifier:@"TrialScreen"];
+    
+    [self.navigationController pushViewController:next animated:YES];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
